a8b84e406da0f01c2729226fe463a7d26f7120e4
sanathkumar-744 <112154986+sanathkumar-744@users.noreply.github.com>
2023-04-14 21:29:36 +0530

    Add files via upload

---
+++ inputf.py
@@ -0,0 +1,13 @@
+
+import sympy 
+from sympy import * 
+from tkinter import *
+from tkinter import *
+from tkinter.ttk import *
+from PIL import ImageTk,Image
+
+global M
+
+#M=Matrix([[1, 0, 1, 3,7,4,2,4], [2, 3, 4, 7,1,2,3,4], [-1, -3, -3, -4,1,5,7,3],[4,6,2,2,1,2,3,4],[5,8,3,6,2,5,2,4],[5,2,3,4,6,8,7,6],[1,2,3,4,5,6,7,8],[8,7,6,5,4,3,2,1]])
+#M=Matrix([[1,3,4], [2,4,9],[9,7,9]]) 
+M=Matrix([[1,5,1],[2,11,5]]) 
